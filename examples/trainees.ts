0f28f70e416edd00837c75731dddf66ddf5e0dcb
Hyunwoo Kim <admin@hyunwoo.kim>
2022-10-17 11:54:46 +0900

    Update and rename trainees.example.ts to trainees.ts

---
+++ examples/trainees.ts
@@ -0,0 +1,12 @@
+import { Trainee } from '@heptacode/military-letter';
+
+export const trainees: Trainee[] = [
+  {
+    name: '정민서',
+    unit: '55사단',
+    birthDate: '2002-01-28',
+    enterDate: '2022-09-20',
+    startDate: '2022-10-10',
+    endDate: '2022-11-01',
+  },
+];
