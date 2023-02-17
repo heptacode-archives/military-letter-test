ccf3fb4315ab1c8c72b1c61a61d6f2da84083eeb
Hyunwoo Kim <admin@hyunwoo.kim>
2023-02-17 18:30:15 +0900

    Update trainees.ts

--- examples/trainees.ts
+++ examples/trainees.ts
@@ -2,11 +2,11 @@ import { Trainee } from '@heptacode/military-letter';
 
 export const trainees: Trainee[] = [
   {
-    name: '정민서',
-    unit: '55사단',
-    birthDate: '2002-01-28',
-    enterDate: '2022-09-20',
-    startDate: '2022-10-10',
-    endDate: '2022-11-01',
+    name: '임성우',
+    unit: '1사단',
+    birthDate: '2002-10-10',
+    enterDate: '2023-02-13',
+    startDate: '2023-02-18',
+    endDate: '2023-03-26',
   },
 ];
